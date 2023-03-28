a24bb535c6daf8e21c04b27fecdc316fc41a920d
Light Yagami <sagnikghosh@zohomail.in>
2023-03-28 19:31:39 +0530

    added package-lock and removed the commented validator(it doesn't work) in mongo.js

--- mongo.js
+++ mongo.js
@@ -8,8 +8,6 @@ mongoose.connect(url)
     .then(() => console.log('connected to the db'))
     .catch(e => console.log('error connecting to the db\n', e))
 
-// const numberValidator = num => /^([0-9]{8,})|(([0-9]{2})-([0-9]{6,}))|(([0-9]{3})-([0-9]{5,}))$/.test(num)
-
 const numberValidator = num => /^[0-9]{8,}$/.test(num) || /^([0-9]{2})-([0-9]{6,})$/.test(num) || /^([0-9]{3})-([0-9]{8,})$/.test(num)
 
 const personSchema = new mongoose.Schema({
